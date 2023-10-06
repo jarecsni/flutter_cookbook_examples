6b7db82089fdc1577faab632ce7fb732d2fb264d
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-10-06 08:57:19 +0100

    page 135, foreground and background decorations

--- lib/chapter4/immutable_widget.dart
+++ lib/chapter4/immutable_widget.dart
@@ -7,12 +7,44 @@ class ImmutableWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-      padding: const EdgeInsets.all(40),
-      child: Container(
-        color: Colors.purple,
-        padding: const EdgeInsets.all(50.0),
-        child: _buildShinyCircle(),
+      decoration: const BoxDecoration(
+        color: Colors.green,
+      ),
+      foregroundDecoration: const BoxDecoration(
+        backgroundBlendMode: BlendMode.colorBurn,
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xAA0d6123),
+            Color(0x00000000),
+            Color(0xAA0d6123),
+          ],
+        ),
+      ),
+      padding: const EdgeInsets.all(80),
+      child: Transform.rotate(
+        angle: 180 / math.pi,
+        child: Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.deepPurple.withAlpha(120),
+                spreadRadius: 4,
+                blurRadius: 15,
+                offset: Offset.fromDirection(1.0, 30),
+              )
+            ],
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: _buildShinyCircle(),
+          ),
+        ),
       ),
     );
   }
