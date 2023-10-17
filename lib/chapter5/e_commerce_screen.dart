81feec02ac6902d71c6af0e3a4f2c4fec37089f1
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-10-17 07:41:16 +0100

    Refactoring widget trees (p189)

---
+++ lib/chapter5/e_commerce_screen.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+
+class ECommerceScreen extends StatelessWidget {
+  const ECommerceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      appBar: _buildAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            _buildToggleBar(),
+            SizedBox(
+                height: 100, child: Image.asset('assets/woman_shopping.jpg')),
+            const DealButtons(),
+            _buildProductTitle(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildProductTitle(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+            child: Image.asset(
+              'assets/textiles.jpg',
+              fit: BoxFit.fill,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Lorem Ipsum',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const Text(
+                      'Dolor sit amet, consectetur adipiscing elit. Quisque faucibus.')
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Row _buildToggleBar() {
+    return const Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Recommended',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Formal Wear',
+            style: TextStyle(
+              color: Colors.white54,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Casual Wear',
+            style: TextStyle(
+              color: Colors.white54,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.purpleAccent,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+      leading: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Icon(Icons.home),
+      ),
+      title: const Text('Let\'s go shopping!'),
+      elevation: 0,
+      actions: const [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(Icons.shopping_cart),
+        )
+      ],
+    );
+  }
+}
+
+class DealButtons extends StatelessWidget {
+  const DealButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 15),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.circular(20)),
+                child: const Center(
+                    child: Text(
+                  'Best Sellers',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.indigoAccent,
+                    borderRadius: BorderRadius.circular(20)),
+                child: const Center(
+                    child: Text(
+                  'Daily Deals',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(height: 15),
+        Row(
+          children: <Widget>[
+            DealButton(),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(20)),
+                child: const Center(
+                    child: Text(
+                  'Last Chance',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(height: 15),
+      ],
+    );
+  }
+}
+
+class DealButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  const DealButton({
+    required this.text,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+            color: Colors.lightGreen, borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: color,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
